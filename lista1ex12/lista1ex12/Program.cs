9349adced128a95477d35ba6ce41485db34aa36f
giulliabasilio <giulliabasilio@gmail.com>
2025-04-30 21:39:44 -0300

    Add files via upload

---
+++ lista1ex12/lista1ex12/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lista1ex12
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double p1, p2, p3, p4, p5, total, pagamento, troco;
+
+            Console.Write("Digite o valor do primeiro produto: ");
+            p1 = double.Parse(Console.ReadLine());
+
+            Console.Write("Digite o valor do segundo produto: ");
+            p2 = double.Parse(Console.ReadLine());
+
+            Console.Write("Digite o valor do terceiro produto: ");
+            p3 = double.Parse(Console.ReadLine());
+
+            Console.Write("Digite o valor do quarto produto: ");
+            p4 = double.Parse(Console.ReadLine());
+
+            Console.Write("Digite o valor do quinto produto: ");
+            p5 = double.Parse(Console.ReadLine());
+
+            total = p1 + p2 + p3 + p4 + p5;
+
+            Console.Write("Digite o valor do pagamento: ");
+            pagamento = double.Parse(Console.ReadLine());
+
+            troco = pagamento - total;
+
+            Console.WriteLine("O total da compra é: R$ {0}", total);
+            Console.WriteLine("O troco a ser devolvido é: R$ {0}", troco);
+        }
+    }
+}
