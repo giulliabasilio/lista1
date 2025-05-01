cec49741607ad3484dd1c3d3457ae30883db4e7e
giulliabasilio <giulliabasilio@gmail.com>
2025-04-30 21:37:34 -0300

    Add files via upload

---
+++ lista1ex11/lista1ex11/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lista1ex11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double x, y, resultado;
+
+            Console.Write("Digite o valor de X: ");
+            x = double.Parse(Console.ReadLine());
+
+            Console.Write("Digite o valor de Y: ");
+            y = double.Parse(Console.ReadLine());
+
+            resultado = Math.Pow(x, y);
+
+            Console.WriteLine("{0} elevado a {1} é: {2}", x, y, resultado);
+        }
+    }
+}
