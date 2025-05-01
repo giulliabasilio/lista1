269c9714805af08bcccb1cad797fcbd6d93d6b51
giulliabasilio <giulliabasilio@gmail.com>
2025-04-30 21:28:36 -0300

    Add files via upload

---
+++ lista1ex9/lista1ex9/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lista1ex9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double diametro, raio, area;
+
+            Console.Write("Digite o diâmetro do círculo: ");
+            diametro = double.Parse(Console.ReadLine());
+
+            raio = diametro / 2;
+            area = Math.PI * raio * raio;
+
+            Console.WriteLine("A área de um círculo com diâmetro {0} é: {1}", diametro, area);
+        }
+    }
+}
